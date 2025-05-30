e6e0fa482420b3046e8b245046caeb4dedd561de
bluez-infomatic <sanjaibluez@gmail.com>
2025-05-30 16:58:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path";
-// https://vite.dev/config/
+import path from 'path'
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve:{
-    alias:{
-      "@": path.resolve(__dirname,"./src"),
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  server: {
+    // Optional but recommended: expose to network
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+})
