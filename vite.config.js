2be0f7151d61c3b5a01ca53aeb783ed0a9bc4aa0
bluez-infomatic <sanjaibluez@gmail.com>
2025-05-30 17:14:54 +0530

    233

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
 export default defineConfig({
-  base: "https://grdurai1412.github.io/Duraipandi_portfolio.com/", // 👈 Replace with your repo name
+  base: "/Duraipandi_portfolio/", // Only the repo name with slashes, NOT full URL
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
