382f80bd111e657d0dbfa354ea684f6d5475b781
bluez-infomatic <sanjaibluez@gmail.com>
2025-05-30 17:01:46 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
+import "./index.css";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-export default App;
+// Render directly inside this file
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
